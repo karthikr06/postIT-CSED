69b587722e6280fb0113720b2712bb4dcbbc7696
Karthik R <RAMACHANDRANKARTHIK123@GMAIL.COM>
2026-06-11 21:31:35 +0530

    Update Post.js

--- backend/Post.js
+++ backend/Post.js
@@ -1,1 +1,7 @@
-
+const mongoose = require('mongoose');
+const PostSchema = new mongoose.Schema({
+    title: String, 
+    desc: String, 
+    link: String
+}, { timestamps: true });
+module.exports = mongoose.model('Post',PostSchema,'Posts');
