31100715850236d480fa793752e4b59751747645
Karthik R <RAMACHANDRANKARTHIK123@GMAIL.COM>
2026-06-11 21:30:37 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config('../.env');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Post = require('./models/Post'); 
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error(err));
+
+app.get('/api/posts', async (req, res) => {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.json(posts);
+});
+
+app.post('/api/posts', async (req, res) => {
+    const newPost = new Post(req.body);
+    await newPost.save();
+    res.status(201).json(newPost);
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
