8f91242842e68c0eb8e6c37f14dc6261756f28fa
mafaaz <mafaazmhd@gmail.com>
2026-06-15 17:10:36 +0530

    search page design done

--- frontend/src/content/src/pages/search/search.jsx
+++ frontend/src/content/src/pages/search/search.jsx
@@ -1,8 +1,13 @@
 import './search.css'
 import Gap from '../../gap';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { LuTextSearch } from "react-icons/lu";
 
 function searchPage(){
+    const displaysearch = () => {};
+
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
             document.title = "Search"
         }, []);
@@ -10,7 +15,16 @@ function searchPage(){
         <Gap />
         {/* Search Page Content */}
         <div className="search-container">
-        <input className="search-input" type="text" placeholder="Search..." />
+        <input 
+            className="search-input" 
+            type="text" 
+            placeholder="Search..." 
+            value={query}
+            onChange={(e) => {setQuery(e.target.value)}}
+        />
+        </div>
+        <div className="search-results">
+            {query === "" && <LuTextSearch className="icon"/>}
         </div>
     </div>
     )
