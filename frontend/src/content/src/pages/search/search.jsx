d0fa6a42a1a49eed1cae09995b1322721bc9c39b
mafaaz <mafaazmhd@gmail.com>
2026-06-15 07:04:39 +0530

    search bar

--- frontend/src/content/src/pages/search/search.jsx
+++ frontend/src/content/src/pages/search/search.jsx
@@ -6,11 +6,13 @@ function searchPage(){
     useEffect(() => {
             document.title = "Search"
         }, []);
-    return(<>
-            <Gap />
+    return(<div className="searchPage">
+        <Gap />
         {/* Search Page Content */}
-        search page
-    </>
+        <div className="search-container">
+        <input className="search-input" type="text" placeholder="Search..." />
+        </div>
+    </div>
     )
 }   
 
